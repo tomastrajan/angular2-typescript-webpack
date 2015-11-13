d37348ecb5fbc435aba9135be7fe72e54a580426
Tomas Trajan <tomas.trajan@gmail.com>
2015-11-13 20:28:29 +0100

    chore(build): various build related adjustments

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Angular 2 Typescript Webpack starter project
-Angular 2 Typescript Webpack starter project
+# Angular 2 Typescript Webpack Starter Project
+Angular 2 Typescript Webpack Starter Project
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import 'reflect-metadata';
 import {bootstrap, Component} from 'angular2/angular2';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>Angular 2 Typescript Webpack Starter Project</h1>'
 })
 class AppComponent { }
 
